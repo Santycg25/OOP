e094ed19cfc1e61b9ce304e0493bfbe09e3b133e
Santycg25 <161526033+Santycg25@users.noreply.github.com>
2024-03-20 12:47:53 -0500

    Add files via upload

---
+++ C++/primerasPracticas/Practica1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class MyClass {       // The class
+  public:             // Access specifier
+    int myNum;        // Attribute (int variable)
+    string myString;  // Attribute (string variable)
+};
+
+int main() {
+  MyClass myObj;  // Create an object of MyClass
+
+  // Access attributes and set values
+  myObj.myNum = 15;
+  myObj.myString = "Un poco de texto...";
+
+  // Print values
+  cout << myObj.myNum << "\n"; 
+  cout << myObj.myString; 
+  return 0;
+}
